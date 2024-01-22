2873bc9462ce1de2cf720c6cf0404e1b45814e59
marouaneTalbi <talbi.918marouane@gmail.com>
2024-01-22 16:23:56 +0100

    mettre a jour les joueurs connectés en temps reel

--- server/controller/roomController.js
+++ server/controller/roomController.js
@@ -27,3 +27,31 @@ export const getRoomByQuizId = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+
+export const getRoomByLink = async (req, res) => {
+    try {
+        const link = req.params.link;
+        const room = await RoomModel.findOne({ where: { link: link } });
+
+        if (!room) {
+            return res.status(404).json({ message: "Room not found for the provided Link." });
+        }
+
+        return res.status(200).json(room);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const changeRoomState = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+        const room = await RoomModel.findByPk(id);
+        room.status = status;
+        await room.save();
+        return res.status(200).json(room);
+    } catch(error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
--- server/route/roomRoute.js
+++ server/route/roomRoute.js
@@ -4,7 +4,8 @@ import * as RoomController from '../controller/roomController.js';
 
 route.post('/room', RoomController.createRoom)
 route.get('/room/:id', RoomController.getRoomByQuizId)
-
+route.get('/roomlink/:link', RoomController.getRoomByLink)
+route.put('/room/:id', RoomController.changeRoomState)
 
 
 export default route;
